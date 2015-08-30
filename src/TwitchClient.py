bf85682af1f498e764111e44f5ee423b85ccd7e3
Benjamin Congdon <bencongdon96@gmail.com>
2015-08-29 23:18:55 -0500

    Moved file to end of execution

--- src/TwitchClient.py
+++ src/TwitchClient.py
@@ -1,6 +1,5 @@
-import socket, string
+import socket, string, os, atexit, sys
 from operator import itemgetter
-import os
 
 HOST = "irc.twitch.tv"
 NAME = "Crowdshoutbot"
@@ -37,6 +36,15 @@ s.send("PASS " + PASS + "\r\n")
 s.send("NICK " + NAME + "\r\n")
 s.send("JOIN #sodapoppin \r\n")
 
+@atexit.register
+def OutputChatData():
+    f = open("output.txt", "w+")
+    temp = sorted(wordsDictionary.items(), key=itemgetter(1))
+    for k in temp:
+        f.write(str(k) + "\n")
+    f.close()
+    print "done!"
+
 while True:
     readbuffer = readbuffer + s.recv(1024)
     temp = string.split(readbuffer, "\n")
@@ -62,20 +70,20 @@ while True:
                 if MODT:
                     #print username + ": " + message
                     words = message.lower().split(" ")
+                    print words
                     for word in words:
                         if wordsDictionary.has_key(word):
                             wordsDictionary[word] = wordsDictionary[word] + 1
-                            print "Repeated word: " + word + " x " + str(wordsDictionary[word])
+                            #print "Repeated word: " + word + " x " + str(wordsDictionary[word])
                         else:
                             if word not in filter:
                                 wordsDictionary[word] = 1
-                                print "New word: " + word
-                    f = open("output.txt", "w+")
-                    sorted(wordsDictionary.items(), key = lambda x: x[1])
-                    for k in wordsDictionary.keys():
-                        f.write(k + " : " + str(wordsDictionary[k]) + "\n")
-                    f.close()
+                                #print "New word: " + word
+                    if len(wordsDictionary) > 100:
+                        sys.exit()
+
 
                 for l in parts:
                     if "End of /NAMES list" in l:
                         MODT = True
+                        print "Connected to Twitch; Listening to chat."
